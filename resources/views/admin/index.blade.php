af4d2de4994bb74f105ce9be2d3f7f89baff6a7e
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-04-22 20:04:27 +0200

    model, migrations, seeder e controller

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,9 @@
     <div class="container py-5">
 
         <h1>Sezione Admin</h1>
-        <h2>Benvenuto {{$user->name}}</h2>
+        <h3>Benvenuto {{$user->name}}</h3>
+
+        @dump($user)
 
     </div>
 
